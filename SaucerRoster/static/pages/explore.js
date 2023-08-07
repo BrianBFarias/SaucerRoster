9160cd5a3a59628878f9823279c8fe3d19853ea8
Brian Farias <Brian03032003@gmail.com>
2023-08-06 20:48:54 -0400

    loading

--- SaucerRoster/static/pages/explore.js
+++ SaucerRoster/static/pages/explore.js
@@ -111,10 +111,10 @@ function back_post(){
 
 document.onreadystatechange = function() {
   if (document.readyState !== "complete") {
-      document.querySelector("body").style.visibility = "none";
       document.querySelector("#loader").style.visibility = "visible";
+      document.querySelector("body").style.visibility = "none";
   } else {
-      document.querySelector("#loader").style.display = "none";
-      document.querySelector("body").style.visibility = "visible";
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
   }
 };
